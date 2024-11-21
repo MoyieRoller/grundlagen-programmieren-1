0980fb7bd4ee4798c0e3cfa61091232c58eb5296
Moyie Roller <moyie.roller@protonmail.com>
2024-11-21 12:13:24 +0100

    cool remove feature added

--- list_sorting.py
+++ list_sorting.py
@@ -2,8 +2,19 @@ ultra_mega_list = []
 while True:
     user_input = input('Please insert list elements: ')
     if user_input.lower() == 'exit':
+        ultra_mega_list.sort()
         break
-    else: ultra_mega_list.append(user_input)
+    elif user_input == 'show_list':
+        print(ultra_mega_list)
+    else:
+        if user_input.startswith('/'):
+            user_input = user_input.lstrip('/')
+            if user_input in ultra_mega_list:
+                index = ultra_mega_list.index(user_input)
+                ultra_mega_list.pop(index)
+            else:
+                print('Item to remove not in list!')
+        else:
+            ultra_mega_list.append(user_input)
 
-ultra_mega_list.sort()
 print(ultra_mega_list)
