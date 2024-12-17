ece997f2e26f80df4b65f9e09cd1144b5bbb525d
Moyie Roller <moyie.roller@protonmail.com>
2024-12-17 10:53:16 +0100

    Vorlesung 17.12.

---
+++ dictionary_stuff.py
@@ -0,0 +1,49 @@
+fruit_prices = {
+    'apple': 1.0,
+    'banana': 0.5,
+    'orange': 0.75,
+}
+
+# print apple price
+print(fruit_prices['apple'])
+print(fruit_prices.get('applle', 0))
+
+# add kiwi price
+fruit_prices['kiwi'] = 0.2
+# fruit_prices.update({'kiwi': 0.2})
+
+# print item count
+print('Total amount of fruit in dic', len(fruit_prices))
+
+# change banana price
+fruit_prices.update({'banana': 0.6})
+print('New banana price:', fruit_prices['banana'])
+
+# delete orange
+fruit_prices.pop('orange')
+
+# check if apple and cherry exist in dictionary
+print('apple' in fruit_prices)
+print('cherry' in fruit_prices)
+
+# total price
+def get_total_price(fruit_list):
+    total_price = 0
+
+    for fruit in fruit_list:
+        if fruit in fruit_prices:
+            total_price += fruit_prices[fruit]
+        else:
+            print(f'Attention, {fruit} not in price list.')
+    return total_price
+
+print('Total fruit price:', get_total_price(['apple', 'banana', 'kiwi']))
+print('Total fruit price:', get_total_price(['apple', 'banana', 'mango']))
+
+# lowest price
+def get_cheapest_fruit(dict):
+    fruit = list(dict.values())
+    fruit.sort()
+    return fruit[0]
+
+print('Lowest price:', get_cheapest_fruit(fruit_prices))
